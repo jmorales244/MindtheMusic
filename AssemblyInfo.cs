e03f50e35813c558b977141e8cb5234cdf7ceb7d
Home PC-VS <jhjmorall@ucsc.edu>
2025-05-03 22:56:27 -0700

    Added assets (logo, etc) and author

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,10 +1,17 @@
 using System.Windows;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
-[assembly:ThemeInfo(
-    ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
-                                                //(used if a resource is not found in the page,
-                                                // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
-                                                //(used if a resource is not found in the page,
-                                                // app, or any theme specific resource dictionaries)
-)]
+
+[assembly: AssemblyTitle("MindTheMusic")]
+[assembly: AssemblyDescription("Automatically mutes or pauses Spotify when a video plays")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jheremy Morales")]
+[assembly: AssemblyProduct("MindTheMusic by Jheremy M.")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("MindTheMusic™")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e5a8d75e-1324-47c2-b8e1-4d7a011bc1fe")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
